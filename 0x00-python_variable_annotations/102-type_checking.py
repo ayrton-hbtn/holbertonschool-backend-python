f7ab778d5c1818de3adb455d58b5c018c73bc73c
ayrton-hbtn <3282@holbertonschool.com>
2022-04-21 20:32:45 -0300

    docstring in function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """return a list of the tuple elements repeated factor times"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
