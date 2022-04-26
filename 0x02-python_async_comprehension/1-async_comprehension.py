ee6d83148aaf6ba6db0efa193c7cabaf295fd2e2
ayrton-hbtn <3282@holbertonschool.com>
2022-04-26 12:24:16 -0300

    async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Async comprehension"""
+
+import asyncio
+from typing import List 
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    collect 10 random numbers using an
+    async comprehensing over async_generator
+    """
+    numbers = [i async for i in async_generator()]
+    return numbers
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Async comprehension"""
+
+import asyncio
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """runs async_comprehension() four times in parallel"""
+    start = time()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    return (time() - start)
