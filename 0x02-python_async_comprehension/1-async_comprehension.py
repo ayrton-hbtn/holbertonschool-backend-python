f871b21a6779c01d797a0981f6c0e4ab548b623a
ayrton-hbtn <3282@holbertonschool.com>
2022-04-26 12:49:13 -0300

    ordered imports

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Async comprehension"""
 
+from typing import List
 import asyncio
-from typing import List 
 
 async_generator = __import__('0-async_generator').async_generator
 
