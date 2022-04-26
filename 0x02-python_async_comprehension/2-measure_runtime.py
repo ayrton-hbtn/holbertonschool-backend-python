fc9c77cf6700716b11788c0dc1422f61ce030086
ayrton-hbtn <3282@holbertonschool.com>
2022-04-26 12:27:50 -0300

    type-annotated

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """runs async_comprehension() four times in parallel"""
     start = time()
     await asyncio.gather(async_comprehension(),
