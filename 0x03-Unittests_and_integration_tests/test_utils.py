cea60870a21cdb592df23a090cf055be8a595f8c
ayrton-hbtn <3282@holbertonschool.com>
2022-05-14 17:31:20 -0300

    assertRaises KeyError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Testing utils.access_nested_map method"""
 
-access_nested_map = __import__('utils').access_nested_map
 from parameterized import parameterized
 import unittest
+access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,3 +16,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, map, path, expected):
         """method to test access_nested_map"""
         self.assertEqual(access_nested_map(map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, map, path, expected):
+        """Checks that a KeyError is raised when wrong inputs are passed"""
+        with self.assertRaises(KeyError) as e:
+            self.assertEqual(access_nested_map(map, path), e.exception)
